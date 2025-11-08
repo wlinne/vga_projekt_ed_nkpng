43a56d203ec5db918c0d7aa5cc852aa669111bd7
wlinne <98419008+wlinne@users.noreply.github.com>
2025-11-08 17:13:17 +0100

    trying to fix test

--- test/test.py
+++ test/test.py
@@ -23,11 +23,11 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
     
-    dut.vgad_0.clk = 0;
-    dut.vgad_0.vgad_0.vgatim_0.hsc_0.vids_0.state = 0;
-    dut.vgad_0.vgad_0.vgatim_0.vsc_0.vids_0.state = 0;
-    dut.vgad_0.vgad_0.vgatim_0.hsc_0.scnt_0.cnt = 0;
-    dut.vgad_0.vgad_0.vgatim_0.vsc_0.scnt_0.cnt = 0;
+    dut.tbvgad_0.clk = 0;
+    dut.user_project.vgad_0.vgad_0.vgatim_0.hsc_0.vids_0.state = 0;
+    dut.user_project.vgad_0.vgad_0.vgatim_0.vsc_0.vids_0.state = 0;
+    dut.user_project.vgad_0.vgad_0.vgatim_0.hsc_0.scnt_0.cnt = 0;
+    dut.user_project.vgad_0.vgad_0.vgatim_0.vsc_0.scnt_0.cnt = 0;
     
     dut._log.info("Test project behavior")
 

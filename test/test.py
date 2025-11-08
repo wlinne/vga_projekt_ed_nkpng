0f0d64a0948ac589aeb62ee1e9157d8d0f2a2919
wlinne <98419008+wlinne@users.noreply.github.com>
2025-11-08 17:27:41 +0100

    remove semicolon

--- test/test.py
+++ test/test.py
@@ -23,11 +23,11 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
     
-    dut.user_project.vgad_0.clk.value = 0;
-    dut.user_project.vgad_0.vgad_0.vgatim_0.hsc_0.vids_0.state.value = 0;
-    dut.user_project.vgad_0.vgad_0.vgatim_0.vsc_0.vids_0.state.value = 0;
-    dut.user_project.vgad_0.vgad_0.vgatim_0.hsc_0.scnt_0.cnt.value = 0;
-    dut.user_project.vgad_0.vgad_0.vgatim_0.vsc_0.scnt_0.cnt.value = 0;
+    dut.user_project.vgad_0.clk.value = 0
+    dut.user_project.vgad_0.vgad_0.vgatim_0.hsc_0.vids_0.state.value = 0
+    dut.user_project.vgad_0.vgad_0.vgatim_0.vsc_0.vids_0.state.value = 0
+    dut.user_project.vgad_0.vgad_0.vgatim_0.hsc_0.scnt_0.cnt.value = 0
+    dut.user_project.vgad_0.vgad_0.vgatim_0.vsc_0.scnt_0.cnt.value = 0
     
     dut._log.info("Test project behavior")
 

1635bf909c858a692ecfea897c9a40f188826280
wlinne <98419008+wlinne@users.noreply.github.com>
2025-11-08 17:45:15 +0100

    fix assert

--- test/test.py
+++ test/test.py
@@ -36,11 +36,12 @@ async def test_project(dut):
     #dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 198) # to pass syn
+    await ClockCycles(dut.clk, 4) # to pass syn
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == LogicArray('10000000', Range(7, 'downto', 0))
+    assert dut.uo_out.value == 136
+    #LogicArray('10000000', Range(7, 'downto', 0))
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
